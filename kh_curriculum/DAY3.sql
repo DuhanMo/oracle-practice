89f2a1d32b6eecbc21b82bedbd532d1e45673b6c
Duhan Mo <devduhanmo@gmail.com>
2020-07-01 16:00:52 +0900

    sql practice

--- kh_curriculum/DAY3.sql
+++ kh_curriculum/DAY3.sql
@@ -1,107 +1,224 @@
 -- DAY3 수업내용
--- NULL 처리함수
--- NVL 함수
--- NVL 함수는 NULL을 0 또는 다른 값으로 변환하기 위해 사용하는 함수
--- NULL을 실제 값으로 변환하기 위해서 사용하는 데이터 유형은 날짜, 문자, 숫자 -> 주의할 점 두 값의 데이터 타입은 일치해야한다
--- NVL(NULL을 포함하는 컬럼 또는 표현식 , NULL을 대체하는 값)
+-- NULL 처리 함수
+-- NVL함수
+-- NVL함수는 NULL을 0또는 다른 값으로 변환하기 위해서 사용하는 함수
+-- NULL을 실제 값으로 변환하기 위해서 사용하는 데이터 유형은 날짜,문자,숫자 -> 주의할점 두 값의 데이터 타입은 일치해야한다.
+-- NVL(NULL을 포함하는 컬럼 또는 표현식, NULL을 대체하는 값)
 SELECT
     EMP_NAME
-,   BONUS
-,   NVL(BONUS, 0)
-FROM
-    EMPLOYEE;
+  , BONUS
+  , NVL(BONUS,'0')
+FROM EMPLOYEE;
 
 -- NVL2함수
--- NVL2(컬럼명, 바꿀값1,바꿀값2)
--- 해당 컬럼이 값이 있으면 바꿀값1 로 변경
--- 해당 컬럼이 NULL이면 바꿀값 2 로 변경
+-- NVL2(컬럼명,바꿀값1,바꿀값2)
+-- 해당 컬럼이 값이 있으면 바꿀값1로 변경,
+-- 해당 컬럼이 NULL이면 바꿀값2로변경
 
 -- 직원정보에서 보너스포인트가 NULL인 직원은 0.5로
 -- 보너스 포인트가 NULL이 아닌 경우 0.7로 변경하여 조회
-
 SELECT
     EMP_NAME
-,   BONUS
-,   NVL2(BONUS,0.7,0.5)
+  , BONUS
+  , NVL2(BONUS,0.7,0.5)
 FROM
     EMPLOYEE;
 
 -- 선택함수
--- ㅇ러가지 경우에 선택할 수 있는 기능을 제공한다
--- DECODE(계산식|컬럼명,조건값1,선택값1,조건값2,선택값2,...)
+-- 여러가지 경우에 선택할 수 있는 기능을 제공한다
+-- DECODE(계산식|컬럼명,조건값1,선택값1,조건값2,선택값2,.....)
 SELECT
     EMP_ID
-,   EMP_NAME
-,   EMP_ID
-,   DECODE(SUBSTR(EMP_NO,8,1),'1','남','2','여')
+  , EMP_NAME
+  , EMP_NO
+  , DECODE(SUBSTR(EMP_NO,8,1),'1','남','2','여')
+FROM
+    EMPLOYEE;
+-- 마지막 인자로 조건값 없이 선택값을 작성하면
+-- 아무것도 해당하지 않을 때 마지막에 작성한 선택값을 무조건 선택한다.
+SELECT
+    EMP_ID
+  , EMP_NAME
+  , EMP_NO
+  , DECODE(SUBSTR(EMP_NO,8,1),'1','남','여')
 FROM
     EMPLOYEE;
 
-SELECT
-    EMP_ID
-,   EMP_NAME
-,   EMP_ID
-,   DECODE(SUBSTR(EMP_NO,8,1),'1','남','여')
-FROM
-    EMPLOYEE;
-
--- CASE WHEN THEN
+-- CASE WEHN THEN
 -- CASE WHEN 조건식 THEN 결과값
 --      WHEN 조건식 THEN 결과값
 --      ELSE 결과값
 -- END
 
+
 SELECT
     EMP_NAME
-,   JOB_CODE
-,   SALARY
-,   CASE WHEN JOB_CODE = 'J7' THEN SALARY * 1.1
-         WHEN JOB_CODE = 'J6' THEN SALARY * 1.15
-        WHEN JOB_CODE = 'J5' THEN SALARY * 1.2
-        ELSE SALARY * 1.05
-    END AS "인상급여"
+  , JOB_CODE
+  , SALARY
+  , CASE WHEN JOB_CODE ='J7' THEN SALARY *1.1
+         WHEN JOB_CODE ='J6' THEN SALARY *1.15
+         WHEN JOB_CODE ='J5' THEN SALARY *1.2
+         ELSE SALARY * 1.05
+    END AS 인상급여
 FROM EMPLOYEE;
 
--- OEDER BY 절 : SELECT 한 컬럼을 가지고 정렬을 할 때 사용함
--- ORDER BY 컬럼명| 컬럼별칭 | 컬럼나열 순번 [ASC]|DESC
--- ORDER BY 컬럼명 정렬방식, 컬럼명 정렬방식, 컬럼명, 정렬방식
+-- ORDER BY 절 : SELECT한 컬럼을 가지고 정렬을 할 떄 사용함
+-- ORDER BY 컬럼명 | 컬럼별칭| 컬럼나열 순번 [ASC]|DESC
+-- ORDER BY 컬럼명 정렬방식, 컬럼명 정렬방식, 컬럼명, 정렬방식....
 -- 첫번째 기준으로 하는 컬럼에 대해 정렬하고,
--- 같은 값들에 대해 뚜번 째 기준으로 하는 컬럼에 대해 다시 정렬
+-- 같은 값들에 대해 두번 쨰 기준으로 하는 컬럼에 대해 다시 정렬
 -- SELECT 구문 맨 마지막에 위치한다.
--- 실행순서도 맨 마지막에 실행된다.
-
+-- 실행순서도 맨마지막에 실행된다.
 SELECT
     EMP_NAME
-,   JOB_CODE
-,   SALARY
-,   CASE WHEN JOB_CODE = 'J7' THEN SALARY * 1.1
-         WHEN JOB_CODE = 'J6' THEN SALARY * 1.15
-        WHEN JOB_CODE = 'J5' THEN SALARY * 1.2
-        ELSE SALARY * 1.05
-    END AS "인상급여"
+  , JOB_CODE
+  , SALARY
+  , CASE WHEN JOB_CODE ='J7' THEN SALARY *1.1
+         WHEN JOB_CODE ='J6' THEN SALARY *1.15
+         WHEN JOB_CODE ='J5' THEN SALARY *1.2
+         ELSE SALARY * 1.05
+    END AS 인상급여
 FROM EMPLOYEE
 ORDER BY 1 DESC;
 
 -- GROUP BY 절 : 같은 값들이 여러개 기록된 컬럼을 가지고
---                 같은 값들을 하나의 그룹으로 묶는다.
--- GROUP BY 컬럼명 | 함수식..
--- 여러개의 값을 묶어서 하나의 그룹으로 처리할 목적으로 사용한다.
+--               같은 값들을 하나의 그룹으로 묶는다.
+-- GROUP BY 컬럼명 | 함수식...
+-- 여러 개의 값을 묶어서 하나의 그룹으로 처리할 목적으로 사용한다.
 -- 그룹으로 묶은 값에 대해서 SELECT 절에서 그룹함수를 사용한다.
 -- HAVING절 : 그룹함수로 구해올 그룹에 대해 조건을 설정할 때 사용
 -- HAVING 컬럼명 | 함수식 비교연산자 비교값
-/*
 
-    5.SELECT 컬럼명 AS 별칭, 계산식, 함수식
-    1.FROM 참조할 테이블명
-    2.WHERE 컬럼명 : 함수식 비교연산자 비교값
-    3.GROUP BY 그룹을 묶을 컬럼명
-    4.HAVING 그룹함수식 비교연산자 비교값
-    6.ORDER BY 컬럼명 | 별칭 | 컬럼순번 정렬방식
+/*
+   5: SELECT 컬럼명 AS 별칭, 계산식, 함수식
+   1: FROM 참조할 테이블명
+   2: WHERE 컬럼명 | 함수식 비교연산자 비교값
+   3: GROUP BY 그룹을 묶을 컬럼명
+   4: HAVING 그룹함수식 비교연산자 비교값
+   6: ORDER BY 컬럼명 | 별칭 | 컬럼순번 정렬방식
 */
 
 SELECT
     COUNT(*) 사원수
-,   DEPT_CODE
+ , DEPT_CODE
 FROM EMPLOYEE
 GROUP BY DEPT_CODE
 ORDER BY 1;
+
+----------------------------------------------------------------
+-- 급여가 300만원이상인 직원이 속한 부서를 고를때
+SELECT DEPT_CODE,FLOOR(AVG(SALARY)) 평균
+FROM EMPLOYEE
+WHERE SALARY > 3000000
+GROUP BY DEPT_CODE
+ORDER BY DEPT_CODE;
+
+-- 급여 평균이 300만원 이상인 부서를 고르느냐
+SELECT DEPT_CODE,FLOOR(AVG(SALARY)) 평균
+FROM EMPLOYEE
+GROUP BY DEPT_CODE
+HAVING FLOOR(AVG(SALARY)) >3000000
+ORDER BY DEPT_CODE;
+
+-- 집계함수
+-- ROLLUP 함수 : 그룹별로 중간 집계 처리를 하는 함수
+-- GROUP BY 절에서만 사용하는 함수
+-- 그룹별로 묶여진 값에 대한 중간 집계와 총 집계를 구할 때 사용한다.
+-- 그룹별로 계산된 결과값들에 대한 총집계가 자동으로 추가된다.
+SELECT JOB_CODE,SUM(SALARY)
+FROM EMPLOYEE
+GROUP BY ROLLUP(JOB_CODE)
+ORDER BY JOB_CODE;
+
+-- CUBE 함수 : 그룹별 산출한 결과를 집계하는 함수
+SELECT JOB_CODE,SUM(SALARY)
+FROM EMPLOYEE
+GROUP BY CUBE(JOB_CODE)
+ORDER BY JOB_CODE;
+
+SELECT JOB_CODE,DEPT_CODE,SUM(SALARY)
+FROM EMPLOYEE
+GROUP BY ROLLUP(JOB_CODE,DEPT_CODE)
+ORDER BY DEPT_CODE;
+SELECT * FROM EMPLOYEE;
+SELECT JOB_CODE,DEPT_CODE,SUM(SALARY)
+FROM EMPLOYEE
+GROUP BY CUBE(JOB_CODE,DEPT_CODE)
+ORDER BY DEPT_CODE;
+
+
+-- GROUPING함수 : ROLLUP이나 CUBE에 의한 산출물이
+-- 인자로 전달받은 컬럼 집합의 산출물이면 0을 반환하고
+-- 아니면 1을 반환하는 함수
+SELECT DEPT_CODE,JOB_CODE,SUM(SALARY),
+       GROUPING(DEPT_CODE) "부서별그룹묶인상태"
+     , GROUPING(JOB_CODE) "직급별그룹묶인상태"
+FROM EMPLOYEE
+GROUP BY CUBE(DEPT_CODE,JOB_CODE)
+ORDER BY DEPT_CODE;
+
+
+SELECT DEPT_CODE,JOB_CODE,SUM(SALARY),
+       CASE WHEN GROUPING(DEPT_CODE) = 0
+                  AND GROUPING(JOB_CODE) = 1
+            THEN '부서별합계'
+            WHEN GROUPING(DEPT_CODE) = 1
+                 AND GROUPING(JOB_CODE) = 0
+            THEN '직급별합계'
+            WHEN GROUPING(DEPT_CODE) =0
+                 AND GROUPING(JOB_CODE) =0
+            THEN '그룹별합계'
+            ELSE '총합계'
+        END AS 구분
+FROM EMPLOYEE
+GROUP BY CUBE(DEPT_CODE,JOB_CODE)
+ORDER BY DEPT_CODE;
+
+--SET OPERATION(집합연산)
+-- UNION : 여러개의 쿼리 결과를 하나로 합치는 연산자
+--          중복된 영역을 제외하여 하나로 합친다.
+SELECT EMP_ID, EMP_NAME,DEPT_CODE,SALARY
+FROM EMPLOYEE
+WHERE DEPT_CODE = 'D5'
+UNION
+SELECT EMP_ID,EMP_NAME,DEPT_CODE,SALARY
+FROM EMPLOYEE
+WHERE SALARY > 3000000;
+
+-- UNIONALL : 여러개의 쿼리를 하나로 합치는 연산자
+--          UNION과의 차이점은 중복영역을 모두 포함시킨다.
+SELECT EMP_ID, EMP_NAME,DEPT_CODE,SALARY
+FROM EMPLOYEE
+WHERE DEPT_CODE = 'D5'
+
+UNION ALL
+
+SELECT EMP_ID,EMP_NAME,DEPT_CODE,SALARY
+FROM EMPLOYEE
+WHERE SALARY > 3000000;
+
+-- INTERSECT : 여러개의 SELECT 한 결과에서 공통부분만 결과로 추출, 교집합과 비슷하다.
+
+SELECT EMP_ID, EMP_NAME,DEPT_CODE,SALARY
+FROM EMPLOYEE
+WHERE DEPT_CODE = 'D5'
+
+INTERSECT
+
+SELECT EMP_ID,EMP_NAME,DEPT_CODE,SALARY
+FROM EMPLOYEE
+WHERE SALARY > 3000000;
+
+
+-- MINUS : 선행 SELECT결과에서 다음 SELECT결과와 겹치는 부분을 제외한 나머지 부분만 추출, 차집합과 비슷
+
+SELECT EMP_ID, EMP_NAME,DEPT_CODE,SALARY
+FROM EMPLOYEE
+WHERE DEPT_CODE = 'D5'
+
+MINUS
+
+SELECT EMP_ID,EMP_NAME,DEPT_CODE,SALARY
+FROM EMPLOYEE
+WHERE SALARY > 3000000;
+
---
+++ kh_curriculum/DAY4.sql
@@ -0,0 +1,312 @@
+-- DAY4 수업내용
+
+-- JOIN : 두개의 테이블을 하나로 합쳐서 결과를 조회
+
+/*
+    하나 이상의 테이블에서 데이터를 조회하기 위해 사용하고 결과는 하나의 RESULT SET으로 나온다.
+    관계형 데이터베이스에서 SQL을 이용해 테이블간 '관계'를 맺는 방법
+    관계형 데이터베이스는 최소한의 데이터를 테이블에 담고 있어(중복을 최소화해서)
+    원하는 정보를 테이블에서 조회하려면 한 개 이상의 테이블에서 데이터를 읽어와야 되는 경우가 많다.
+    
+    이때, 무작정 데이터르 가져오는게 아닌 테이블간 연결고리로 관계를 맺어진 데이터를 추출해야한다.
+    --> JOIN을 통해 이를 구현가능하다.
+
+    JOIN은 크게 오라클 구문과 ANSI 구문
+    ANSI는 미국국립표준협희(American National Standards Institute)를 뜻한다.
+    ANSI 구문 같은 경우 오라클 DBMS외의 다른 DBMS에서 똑같이 사용된다.
+
+        오라클 구문          |           SQL:1999년표준(ANSI)구문
+----------------------------------------------------------------------------
+          등가 조인          |      내부조인(INNER JOIN)  --> JOIN  USING/ON
+                             |      자연조인(NATURAL JOIN)--> JOIN USING 구문과 비슷
+------------------------------------------------------------------------------------
+          포괄조인           |      왼쪽 외부 조인 (LEFT OUTER JOIN)
+        (LEFT OUTER)         |      오른쪽 외부 조인(RIGHT OUTER JOIN)
+        (RIGHT OUTER)        |      전체 외부 조인(FULL OUTER JOIN)--> 오라클 구문으로 사용 못함
+*/
+
+-- 오라클 전용 구문
+-- FROM절에 ','로 구분하여 합치게 될 테이블명을 기술하고
+-- WHERE절에 합치기에 사용할 컬럼명을 명시한다.
+-- 연결에 사용할 두 컬럼명이 다른 경우
+SELECT EMP_ID,EMP_NAME,DEPT_CODE,DEPT_TITLE
+FROM EMPLOYEE,DEPARTMENT
+WHERE DEPT_CODE = DEPT_ID;
+
+-- 연결에 사용할 두 컬럼명이 같은 경우
+SELECT EMP_ID,EMP_NAME,EMPLOYEE.JOB_CODE,JOB_NAME
+FROM EMPLOYEE,JOB
+WHERE EMPLOYEE.JOB_CODE = JOB.JOB_CODE;
+
+-- 테이블에 별칭 사용
+SELECT EMP_ID,EMP_NAME,E.JOB_CODE,JOB_NAME
+FROM EMPLOYEE E,JOB J
+WHERE E.JOB_CODE = J.JOB_CODE;
+
+-- ANSI표준 구문
+-- 연결에 사용할 컬럼명이 같은 경우 USING(컬럼명)을 사용
+SELECT EMP_ID,EMP_NAME,JOB_CODE,JOB_NAME
+FROM EMPLOYEE 
+JOIN JOB USING(JOB_CODE);
+
+-- 연결에 사용할 컬럼명이 다른 경우 ON()을 사용
+SELECT EMP_ID,EMP_NAME,DEPT_CODE,DEPT_TITLE
+FROM EMPLOYEE 
+JOIN DEPARTMENT ON(DEPT_CODE = DEPT_ID);
+
+-- 연결에 사용할 컬럼명이 같은 경우 ON()을 사용할 수 있다.
+SELECT EMP_ID,EMP_NAME,E.JOB_CODE,JOB_NAME
+FROM EMPLOYEE E 
+JOIN JOB J ON(E.JOB_CODE = J.JOB_CODE);
+
+-- 부서테이블과 지역테이블을 조인하여 테이블에 모든 데이터를 조회
+-- ANSI 표준
+SELECT *
+FROM DEPARTMENT
+JOIN LOCATION ON(LOCATION_ID= LOCAL_CODE);
+
+-- 오라클 전용
+SELECT *
+FROM DEPARTMENT D,LOCATION L
+WHERE D.LOCATION_ID = L.LOCAL_CODE;
+
+-- 조인은 기본이 EQUAL JOIN : 연결되는 컬럼의 값이 일치하는 행들만 조인된다.
+-- 일치하는 값이 없는 행은 조인에서 제외되는 것을 INNER JOIN이라고한다.
+-- JOIN의 기본은 INNER JOIN & EQAUL JOIN이다.
+
+-- OUTER JOIN : 두 테이블의 지정하는 컬럼 값이 일치하지 않는 행도 조인에 포함을 시킨다.
+--              반드시 OUTER JOIN임을 명시해야한다.
+
+-- 1. LEFT OUTER JOIN : 합치기에 사용한 두 테이블 중 왼쪽편에 기술된 테이블의 행의 수를 기준으로 JOIN
+-- 2. RIGHT OUTER JOIN : 합치기에 사용한 두 테이블 중 오른편에 기술된 테이블의 행의 수를 기준으로 JOIN
+-- 3. FULL OUTER JOIN
+
+SELECT EMP_NAME,DEPT_TITLE
+FROM EMPLOYEE
+JOIN DEPARTMENT ON (DEPT_CODE = DEPT_ID);
+
+-- LEFT OUTER JOIN
+-- ANSI 표준
+SELECT EMP_NAME,DEPT_TITLE
+FROM EMPLOYEE
+--LEFT OUTER JOIN DEPARTMENT ON (DEPT_CODE = DEPT_ID);
+LEFT JOIN DEPARTMENT ON (DEPT_CODE = DEPT_ID);
+
+-- 오라클 전용 구문
+SELECT EMP_NAME,DEPT_TITLE
+FROM EMPLOYEE,DEPARTMENT
+WHERE DEPT_CODE = DEPT_ID(+);
+
+-- RIGHT OUTER JOIN
+-- ANSI 표준
+SELECT EMP_NAME,DEPT_TITLE, J.JOB_NAME
+FROM EMPLOYEE E
+RIGHT OUTER JOIN DEPARTMENT ON (DEPT_CODE = DEPT_ID)
+JOIN JOB J ON (J.JOB_CODE = E.JOB_CODE);
+--RIGHT JOIN DEPARTMENT ON (DEPT_CODE = DEPT_ID);
+
+-- 오라클 전용
+SELECT EMP_NAME,DEPT_TITLE
+FROM EMPLOYEE,DEPARTMENT
+WHERE DEPT_CODE(+) = DEPT_ID;
+
+-- FULL OUTER JOIN
+-- ANSI 표준
+SELECT EMP_NAME,DEPT_TITLE
+FROM EMPLOYEE
+FULL OUTER JOIN DEPARTMENT ON (DEPT_CODE = DEPT_ID);
+
+-- 오라클 전용구문
+-- 오라클 전용 구문으로는 FULL OUTER JOIN을 하지 못한다.
+SELECT EMP_NAME,DEPT_TITLE
+FROM EMPLOYEE,DEPARTMENT
+WHERE DEPT_CODE(+) = DEPT_ID(+);
+
+-- CROSS JOIN : 카테이션곱이라고도 한다.
+--              조인되는 테이블의 각 행들이 모두 매핑된 데이터가 검색되는 방법
+SELECT EMP_NAME, DEPT_TITLE
+FROM EMPLOYEE
+CROSS JOIN DEPARTMENT;
+
+-- NON EQUAL JOIN
+-- 지정한 컬럼의 값이 일치하는 경우가 아닌, 값의 범위에 포함되는 행들을 연결하는 방식
+-- ANSI표준
+SELECT EMP_NAME,SALARY,E.SAL_LEVEL,S.SAL_LEVEL
+FROM EMPLOYEE E
+JOIN SAL_GRADE S ON(SALARY BETWEEN MIN_SAL AND MAX_SAL);
+-- 오라클 전용
+SELECT EMP_NAME,SALARY,E.SAL_LEVEL,S.SAL_LEVEL
+FROM EMPLOYEE E,SAL_GRADE S
+WHERE SALARY BETWEEN MIN_SAL AND MAX_SAL;
+
+-- SELF JOIN : 같은 테이블을 조인하는 경우
+--             자기 자신과 조인을 맺는 것
+-- 오라클 전용
+SELECT 
+    E1.EMP_ID
+ ,  E1.EMP_NAME 사원이름
+ ,  E1.DEPT_CODE
+ ,  E1.MANAGER_ID
+ ,  E2.EMP_NAME 관리자이름
+FROM EMPLOYEE E1,EMPLOYEE E2
+WHERE E1.MANAGER_ID = E2.EMP_ID;
+
+
+-- ANSI 표준
+SELECT 
+    E1.EMP_ID
+ ,  E1.EMP_NAME 사원이름
+ ,  E1.DEPT_CODE
+ ,  E1.MANAGER_ID
+ ,  E2.EMP_NAME 관리자이름
+FROM EMPLOYEE E1
+JOIN EMPLOYEE E2 ON(E1.MANAGER_ID = E2.EMP_ID);
+
+-- 다중 JOIN : N개의 테이블을 조회할 때 사용
+-- ANSI 표준
+-- 조인 순서 중요함
+SELECT EMP_ID,EMP_NAME,DEPT_CODE,DEPT_TITLE,LOCAL_NAME
+FROM EMPLOYEE
+JOIN DEPARTMENT ON(DEPT_CODE = DEPT_ID)
+JOIN LOCATION ON(LOCATION_ID = LOCAL_CODE);
+
+-- 오라클 전용
+-- 조인 순서 상관없음
+SELECT EMP_ID,EMP_NAME,DEPT_CODE,DEPT_TITLE,LOCAL_NAME
+FROM EMPLOYEE,DEPARTMENT,LOCATION
+WHERE 
+LOCATION_ID = LOCAL_CODE AND DEPT_CODE = DEPT_ID;
+
+
+-- 직급이 대리이면서 아시아 지역에 근무하는 직원 조회
+-- 사번, 이름, 직급명,부서명,근무지역명,급여를 조회하세요.
+SELECT 
+    EMP_ID
+  , EMP_NAME
+  , JOB_NAME
+  , DEPT_TITLE
+  , LOCAL_NAME
+  , SALARY
+FROM 
+    EMPLOYEE E
+  JOIN JOB J ON(E.JOB_CODE = J.JOB_CODE)
+  JOIN DEPARTMENT ON (DEPT_CODE = DEPT_ID)
+  JOIN LOCATION ON(LOCATION_ID = LOCAL_CODE)
+WHERE  JOB_NAME = '대리'
+  --E.JOB_CODE = 'J6'
+--AND LOCAL_NAME = 'ASIA1';
+AND LOCAL_NAME LIKE 'ASIA%';
+
+-- 오라클 전용
+SELECT 
+    EMP_ID
+  , EMP_NAME
+  , JOB_NAME
+  , DEPT_TITLE
+  , LOCAL_NAME
+  , SALARY
+FROM 
+    EMPLOYEE E,JOB J,DEPARTMENT,LOCATION
+WHERE 
+    E.JOB_CODE = J.JOB_CODE
+AND DEPT_CODE = DEPT_ID
+AND LOCATION_ID = LOCAL_CODE
+AND JOB_NAME = '대리'
+AND LOCAL_NAME LIKE 'ASIA%';
+
+
+-- SUBQUERY(서브쿼리)
+--    하나의 SQL문 안에 포함된 또다른 SQL문
+--    메인쿼리(기존쿼리)를 위해 보조 역할을 하는 쿼리문
+
+-- 간단한 서브쿼리 예시 1.
+-- 부서코드가 노옹철 사원과 같은 부서의 직원 명단을 조회해보자
+
+-- 1) 사원명이 노옹철인 사람의 부서코드 조회
+SELECT 
+    DEPT_CODE
+FROM
+    EMPLOYEE
+WHERE
+   EMP_NAME ='노옹철'; -- > D9인걸 찾음
+
+-- 2) 부서코드가 D9인 직원을 조회
+SELECT 
+    EMP_NAME
+FROM
+    EMPLOYEE
+WHERE 
+    DEPT_CODE = 'D9';
+
+--> 위의 2개의 단계를 하나의 쿼리로 
+SELECT 
+    EMP_NAME
+FROM
+    EMPLOYEE
+WHERE 
+    DEPT_CODE = (SELECT 
+                     DEPT_CODE
+                 FROM
+                     EMPLOYEE
+                 WHERE
+                     EMP_NAME ='노옹철');
+
+-- JOIN 연습문제
+--1. 주민번호가 70년대 생이면서 성별이 여자이고, 
+--   성이 전씨인 직원들의 사원명, 주민번호, 부서명, 직급명을 조회하시오.
+
+--   ANSI 표준
+SELECT
+EMP_NAME
+,   EMP_NO
+,   DEPT_TITLE
+,   JOB_NAME
+FROM EMPLOYEE E
+JOIN DEPARTMENT ON (DEPT_CODE = DEPT_ID)
+JOIN JOB J ON ( E.JOB_CODE = J.JOB_CODE)
+WHERE SUBSTR(EMP_NO,1,2) >= 70
+AND SUBSTR(EMP_NO,1,2) <80
+AND SUBSTR(EMP_NO,8,1) = 2;
+
+--   오라클 전용
+
+--2. 가장 나이가 적은 직원의 사번, 사원명, 
+--   나이, 부서명, 직급명을 조회하시오.
+--   ANSI 표준
+SELECT EMP_ID
+     , EMP_NAME
+     , EXTRACT(YEAR FROM SYSDATE)
+     - EXTRACT(YEAR FROM (TO_DATE(SUBSTR(EMP_NO,1,2),'PR'))) + 1 AS 나이
+     , DEPT_TITLE
+     , JOB_NAME
+FROM EMPLOYEE E
+JOIN DEPARTMENT ON ( DEPT_CODE = DEPT_ID)
+JOIN JOB J ON (E.JOB_CODE = J.JOB_CODE)
+WHERE EXTRACT(YEAR FROM SYSDATE)
+     - EXTRACT(YEAR FROM(TO_DATE(SUBSTR(EMP_NO,1,2),'PR'))) + 1
+     = (SELECT MIN(EXTRACT(YEAR FROM SYSDATE)
+                       - EXTRACT(YEAR FROM(TO_DATE(SUBSTR(EMP_NO,1,2),'PR')))+1)
+         FROM EMPLOYEE)
+
+
+
+--  오라클전용
+
+--3. 이름에 '형'자가 들어가는 직원들의
+--   사번, 사원명, 부서명을 조회하시오.
+--   ANSI 표준
+
+--   오라클전용
+
+--4. 해외영업팀에 근무하는 사원명, 
+--   직급명, 부서코드, 부서명을 조회하시오.
+--   ANSI표준
+
+--   오라클전용
+
+--5. 보너스포인트를 받는 직원들의 사원명, 
+--   보너스포인트, 부서명, 근무지역명을 조회하시오.
+--   ANSI표준
+
+
+--   오라클전용
